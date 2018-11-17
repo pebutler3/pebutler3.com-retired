2acb7db3e2ca1a3f5e8f7befc21caeed46e38485
Paul Butler III <p.e.butler3@gmail.com>
2018-11-16 21:24:55 -0700

    Updated family pic and increased size.

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './index.css';
-import me from './me.jpg';
+import family from './family.png';
 import ribbon from './ribbon.svg';
 
 const publicReposUrl = 'https://github.com/pebutler3?tab=repositories';
@@ -9,7 +9,7 @@ const publicReposUrl = 'https://github.com/pebutler3?tab=repositories';
 const Bio = ({...props}) => {
   return (
     <section className="info-box info-box--bio">
-      <h2>[ {props.title} ]</h2>
+      <h2>[{props.title}]</h2>
       <p>I am a self-taught front-end developer from Ohio.</p>
       <p>Father of 2 girls with a hot lady to tie it all together.</p>
       <div className="leukemia-section">
@@ -19,7 +19,7 @@ const Bio = ({...props}) => {
       <p><span>Location:</span> {props.location}, Colorado</p>
       <p><span>Public Repos:</span> <a href={publicReposUrl}>{props.repos}</a></p>
       <div className="avatar__wrapper">
-        <img className="avatar" src={me} alt="P.E. Butler III"/>
+        <img className="avatar" src={family} alt="P.E. Butler III"/>
       </div>
     </section>
   );
